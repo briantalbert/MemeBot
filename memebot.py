13b69ecd9556cd838f68adb5e197129a8029a1b1
briantalbert <43411780+briantalbert@users.noreply.github.com>
2018-09-21 09:31:36 -0400

    Add files via upload

--- memebot.py
+++ memebot.py
@@ -9,20 +9,18 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("memeeconomy")
 inbox = reddit.inbox.comment_replies()
 total = 1000
-hours = 1.0
+y_balance = []
+x_time = []
 bot_string = ' | investments made by memebot pm me if things get out of control'
 
 def MemeBot():
-    while total >= 1000:
-        get_balance()
-        check_messages()
-        invest()
-        create_graph()
+    get_balance()
+    invest()
 
 def create_graph():
     global total
-    y_balance = []
-    x_time = []
+    global y_balance
+    global x_time
     
     y_balance.append(total)
     x_time.append(time.strftime("%H:%M"))
@@ -38,54 +36,46 @@ def create_graph():
     
 def delete_messages():
     x = 0
-    for comment in reddit.redditor('Talbertross').comments.new(limit=None):
+    for comment in reddit.redditor('Talbertross').comments.new(limit=20):
         if comment.body[0] == '!':
             x = x + 1
             comment.delete()
     print('Deleted ' + str(x) + ' comments.')
 
 def get_balance():
+    global total
     x = 0
     for submission in subreddit.rising():
         for toplevel in submission.comments:
             if x == 0:
+                x = 1
                 print("Requesting balance...")
-                toplevel.reply("!balance" + bot_string)
-                x = x + 1
+                replyid = toplevel.reply("!balance" + bot_string)
                 time.sleep(5)
+                
+                
+    read_balance_reply(replyid)
+    
+
+def read_balance_reply(replyid):
+    global total
+    num = ''
+    replyid.refresh()
+    for reply in replyid.replies.list():
+        for letter in reply.body:
+            if letter.isdigit():
+                num = num + letter
+    print('Current total: ' + num + ' Memecoins')
+    total = int(num)
 
 def mark_read():
     for message in inbox:
         if message.author.name == 'MemeInvestor_bot':
             message.mark_read()
 
-        
-def check_messages():
-    num = ''
-    global total
-    num = read_messages()
-    total = int(num)
-
-def read_messages():
-    num = ''
-    global total
-    x = 0
-    for message in inbox:
-        if x == 0 and message.author.name == 'MemeInvestor_bot':
-            message.mark_read()
-            x = 1
-            if 'Currently' in message.body:
-                print(message.body)
-                for letter in message.body:
-                    if letter.isdigit():
-                        num = num + letter
-    if num == '':
-        num = total
-    return num
 
 def invest():
     global total
-    global hours
     invest_amt = total // 5
     if invest_amt < 100:
         invest_amt = 100
@@ -94,7 +84,7 @@ def invest():
     
     for submission in subreddit.rising():
         if submission.score >= 50:
-            #print(submission.title)
+            print(submission.title)
             submissions.append(submission.id)
             x = 0
             submission.downvote()
@@ -116,10 +106,10 @@ def invest():
         print('\n')
         hours = 1
     elif memecount > 0 and memecount < 5:
-        print('Invested in ' + str(memecount) + ' memes. Checking again in 2 hours.')
+        print('Invested in ' + str(memecount) + ' meme(s). Checking again in 1 hour.')
         print("Current time: " + time.strftime("%H:%M") + ".")
         print('\n')
-        hours = 2
+        hours = 1
     elif memecount >= 5:
         print('Invested in ' + str(memecount) + ' memes. Checking again in 4 hours.')
         print("Current time: " + time.strftime("%H:%M") + ".")
@@ -128,6 +118,11 @@ def invest():
 
     delete_messages()
     mark_read()
+    create_graph()
     time.sleep(hours * 3600)
 
-MemeBot()
+while total >= 1000:
+    MemeBot()
+
+
+print("Temporarily bankrupt. Halting investments.")
